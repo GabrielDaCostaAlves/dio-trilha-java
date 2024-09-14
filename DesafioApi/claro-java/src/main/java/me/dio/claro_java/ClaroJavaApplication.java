62791a339a619042131b3d1493cf45494fdb272a
Gabriel Da Costa Alves <109757880+GabrielDaCostaAlves@users.noreply.github.com>
2024-09-14 03:01:32 -0300

    Desafio Api

---
+++ DesafioApi/claro-java/src/main/java/me/dio/claro_java/ClaroJavaApplication.java
@@ -0,0 +1,13 @@
+package me.dio.claro_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaroJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaroJavaApplication.class, args);
+	}
+
+}
---
+++ DesafioApi/claro-java/src/test/java/me/dio/claro_java/ClaroJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.claro_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaroJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
